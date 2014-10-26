2f77f51e407fe53e66bba9ed3fcd16bed77cd5d3
Grace Liang <acie.liang@gmail.com>
2014-10-26 16:12:21 -0700

    Implemented parser
    Implemented parser method, but did not test because there is a 503
    error when deploying.

--- ParkFinder/src/com/google/gwt/parkfinder/client/ParkFinder.java
+++ ParkFinder/src/com/google/gwt/parkfinder/client/ParkFinder.java
@@ -1,35 +1,48 @@
 package com.google.gwt.parkfinder.client;
 
-import com.google.gwt.parkfinder.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
- * Entry point classes define <code>onModuleLoad()</code>.
+ * Entry point classes define onModuleLoad()
  */
 public class ParkFinder implements EntryPoint {
-
 	
-	
-	
+	VerticalPanel mainPanel = new VerticalPanel();
+	TextBox errorMessage = new TextBox();
+	Button startParseButton = new Button("Start Parsing");
 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		
+		mainPanel.add(errorMessage);
+		mainPanel.add(startParseButton);
+		errorMessage.setText("No errors so far");
+		
+		startParseButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				
+				
+				
+				try {
+					SAXParserDemo.parse();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					errorMessage.setText("Parser error");
+					e.printStackTrace();
+				}
+			}
+		});
+		
 
+		
+		
+		
 	}
 }
