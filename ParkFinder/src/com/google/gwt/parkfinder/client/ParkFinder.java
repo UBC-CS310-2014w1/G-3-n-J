691d1ec3d509846abecdb475d900d7f10b071450
Grace Liang <acie.liang@gmail.com>
2014-10-28 14:52:13 -0700

    Added map
    Map displays on page with default settings

--- ParkFinder/src/com/google/gwt/parkfinder/client/ParkFinder.java
+++ ParkFinder/src/com/google/gwt/parkfinder/client/ParkFinder.java
@@ -1,5 +1,11 @@
 package com.google.gwt.parkfinder.client;
 
+import com.google.gwt.maps.client.InfoWindowContent;
+import com.google.gwt.maps.client.MapWidget;
+import com.google.gwt.maps.client.Maps;
+import com.google.gwt.maps.client.control.LargeMapControl;
+import com.google.gwt.maps.client.geom.LatLng;
+import com.google.gwt.maps.client.overlay.Marker;
 import com.google.gwt.parkfinder.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
@@ -11,25 +17,68 @@ import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+import com.google.gwt.dom.client.Style.Unit;
+
+
+
+
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class ParkFinder implements EntryPoint {
 
-	
-	
-	
+
+
+
 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		/*
+		 * Asynchronously loads the Maps API.
+		 *
+		 * The first parameter should be a valid Maps API Key to deploy this
+		 * application on a public server, but a blank key will work for an
+		 * application served from localhost.
+		 */
+		Maps.loadMapsApi("", "2", false, new Runnable() {
+			public void run() {
+				buildUi();
+			}
+		});
+	}
+
+	private void buildUi() {
+		// Open a map centered on Cawker City, KS USA
+		LatLng cawkerCity = LatLng.newInstance(39.509, -98.434);
+
+		final MapWidget map = new MapWidget(cawkerCity, 2);
+		map.setSize("100%", "100%");
+		// Add some controls for the zoom level
+		map.addControl(new LargeMapControl());
+
+		// Add a marker
+		map.addOverlay(new Marker(cawkerCity));
+
+		// Add an info window to highlight a point of interest
+		map.getInfoWindow().open(map.getCenter(),
+				new InfoWindowContent("World's Largest Ball of Sisal Twine"));
+
+		final DockLayoutPanel dock = new DockLayoutPanel(Unit.PX);
+		dock.addNorth(map, 500);
+
+		// Add the map to the HTML host page
+		RootPanel.get().add(dock);
+
+
 
 	}
 }
