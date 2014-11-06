3d693e907be688162b11e89a27935c451acfadbb
Josh Parkes <joshparkes24@gmail.com>
2014-11-06 12:42:24 -0800

    implements facility

--- ParkFinder/src/com/google/gwt/parkfinder/server/ParkServiceImpl.java
+++ ParkFinder/src/com/google/gwt/parkfinder/server/ParkServiceImpl.java
@@ -37,15 +37,17 @@ public class ParkServiceImpl extends RemoteServiceServlet implements ParkService
 		try {
 
 			URL PARK_CSV = new URL("http://m.uploadedit.com/b041/1414532771299.txt");
+			URL FACILITIES_CSV = new URL("http://m.uploadedit.com/b042/1415280168943.txt");
+			URL WASHROOMS_CSV = new URL("http://m.uploadedit.com/b042/1415280351920.txt");
 			BufferedReader in = new BufferedReader(new InputStreamReader(PARK_CSV.openStream()));
 			CSVReader reader = new CSVReader(in);
+			// Skip header row
 			reader.readNext();
 
 			// Clear the old list of park
 			pm.evictAll();
 
 			String[] nextLine = null;
-			// skip header row
 			while ((nextLine = reader.readNext()) != null) {
 				Park park = new Park();
 				park.setParkID(nextLine[0]);
@@ -55,6 +57,10 @@ public class ParkServiceImpl extends RemoteServiceServlet implements ParkService
 				park.setGoogleMapDest(nextLine[7]);
 				park.setNeighbourhoodName(nextLine[9]);
 				park.setNeighbourhoodURL(nextLine[10]);
+				park.setFacility(getFacility(park.getParkID()));
+				System.out.println(park.getParkID());
+				System.out.println(park.getFacility());
+				//System.out.println(park.getFacility()[0].getFacilityType());
 
 				pm.makePersistent(park);
 			}
@@ -63,6 +69,31 @@ public class ParkServiceImpl extends RemoteServiceServlet implements ParkService
 		} finally {
 			pm.close();
 		}
+	}
+
+	public ArrayList<Facility> getFacility(String parkID) throws IOException {
+		List<Facility> facilities = new ArrayList<Facility>();
+		URL FACILITIES_CSV = new URL("http://m.uploadedit.com/b042/1415280168943.txt");
+		BufferedReader inFacility = new BufferedReader(new InputStreamReader(FACILITIES_CSV.openStream()));
+		CSVReader readerFacility = new CSVReader(inFacility);
+		// Skip header row
+		readerFacility.readNext();
+		String[] nextLineFacility = null;
+		while ((nextLineFacility = readerFacility.readNext()) != null) {
+			if (nextLineFacility[0].toString().equals(parkID.toString())) {
+				System.out.println(nextLineFacility[0].toString().equals(parkID.toString()));
+				System.out.println("Facility ID:" + nextLineFacility[0]);
+				System.out.println("ParkID:" + parkID);
+				Facility facility = new Facility();
+				facility.setFacilityCount(nextLineFacility[1]);
+				facility.setFacilityType(nextLineFacility[2]);
+				facilities.add(facility);
+				System.out.println(facility.getFacilityCount() + ":" + facility.getFacilityType());
+			}
+		}
+		readerFacility.close();
+		System.out.println(facilities);
+		return (ArrayList<Facility>) facilities;
 	}
 
 	@Override
